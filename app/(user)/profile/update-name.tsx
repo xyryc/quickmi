2475ca569bfea557a7af43ae177bb6bd465520fd
mdalifkhandev <mdalifk2002@gmail.com>
2025-12-22 05:25:45 +0600

    update name screen complite

--- app/(user)/profile/update-name.tsx
+++ app/(user)/profile/update-name.tsx
@@ -1,12 +1,21 @@
+import ButtonPrimary from "@/components/ButtonPrimary";
 import ScreenHeader from "@/components/ScreenHeader";
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { ScrollView, StatusBar, Text } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StatusBar,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const UpdateName = () => {
   return (
-    <SafeAreaView className="flex-1 mb-28" edges={["top", "left", "right"]}>
+    <SafeAreaView className="flex-1" edges={["top", "left", "right"]}>
       <StatusBar backgroundColor="#D3E6FF" barStyle="dark-content" />
 
       <LinearGradient
@@ -14,14 +23,37 @@ const UpdateName = () => {
         locations={[0, 0.4]}
         style={{ flex: 1 }}
       >
-        {/* header */}
-        <ScreenHeader title="Update your your name" />
+        <ScreenHeader title="Update your name" />
 
-        <ScrollView className="mx-5" showsVerticalScrollIndicator={false}>
-          <Text className="mt-5 font-sf-pro-regular text-sm">
-            Please enter your name as it appears on your ID or {"\n"}passport.
-          </Text>
-        </ScrollView>
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 0}
+        >
+          {/* scrollable content */}
+          <ScrollView
+            className="flex-1 mx-5"
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 120 }}
+          >
+            <Text className="mt-5 text-sm">
+              Please enter your name as it appears on your ID or {"\n"}passport.
+            </Text>
+
+            <Text className="mt-3.5 text-sm font-sf-pro-medium">
+              First Name
+            </Text>
+            <TextInput className="mt-2 p-4 border border-[#E3E6F0] rounded-xl" />
+
+            <Text className="mt-5 text-sm font-sf-pro-medium">Last Name</Text>
+            <TextInput className="mt-2 p-4 border border-[#E3E6F0] rounded-xl" />
+          </ScrollView>
+
+          {/* bottom button */}
+          <View className="px-5 pb-28">
+            <ButtonPrimary title="Save" />
+          </View>
+        </KeyboardAvoidingView>
       </LinearGradient>
     </SafeAreaView>
   );
