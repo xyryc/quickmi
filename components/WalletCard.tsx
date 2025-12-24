8f6e4f9f5e146f302b20ff75a6c7289b06ac28e2
mdalifkhandev <mdalifk2002@gmail.com>
2025-12-25 01:19:22 +0600

    wallet screen conplite

--- components/WalletCard.tsx
+++ components/WalletCard.tsx
@@ -1,19 +1,35 @@
 import { ImageBackground } from "expo-image";
 import React from "react";
 import { Text, View } from "react-native";
+import ButtonSecondary from "./ButtonSecondary";
 
 const WalletCard = () => {
   return (
-    <View className="items-center justify-center">
+    <View className=" mt-2.5">
+      {/* card image */}
       <ImageBackground
         source={require("@/assets/images/wallet-card.png")}
         contentFit="contain"
         style={{ width: "100%", height: 180 }}
         className="items-center justify-center "
       >
-        <Text className="text-lg font-bold text-black">Welcome</Text>
-
-        <Text className="text-xs text-gray-700">Expo + NativeWind</Text>
+        <View className="flex-1 justify-center">
+          <View className="flex-row items-center justify-between pr-2 pl-5">
+            <View>
+              <Text className="text-white text-2xl font-sf-pro-medium">
+                Available Balance{" "}
+              </Text>
+              <Text className="text-white text-5xl font-sf-pro-semibold mt-2">
+                $ 2652
+              </Text>
+            </View>
+            <View>
+              {/* buttons */}
+              <ButtonSecondary title={"Deposits"} className={"px-8"} />
+              <ButtonSecondary title={"Cash Out"} className={"px-8 mt-2"} />
+            </View>
+          </View>
+        </View>
       </ImageBackground>
     </View>
   );
