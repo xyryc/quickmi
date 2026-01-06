54c3722adc46d2cd65a9980d04b27867944ba089
xyryc <amdanikzz@gmail.com>
2026-01-07 02:51:14 +0600

    fixed header positioning issue

--- components/ScreenHeader.tsx
+++ components/ScreenHeader.tsx
@@ -8,16 +8,18 @@ const ScreenHeader = ({ title }) => {
 
   return (
     <View className="py-3 px-5 flex-row items-center">
-      <TouchableOpacity
-        onPress={() => router.back()}
-        className="w-11 h-11 bg-white items-center justify-center rounded-full border-[0.5px] border-[#0F73F7E5]"
-      >
-        <MaterialIcons name="keyboard-arrow-left" size={24} color="black" />
-      </TouchableOpacity>
-
-      <View className="flex-1 items-center">
-        <Text className="text-xl font-sf-pro-medium">{title}</Text>
+      <View className="flex-1">
+        <TouchableOpacity
+          onPress={() => router.back()}
+          className="w-11 h-11 bg-white items-center justify-center rounded-full border-[0.5px] border-[#0F73F7E5]"
+        >
+          <MaterialIcons name="keyboard-arrow-left" size={24} color="black" />
+        </TouchableOpacity>
       </View>
+
+      <Text className="text-xl font-sf-pro-medium">{title}</Text>
+
+      <View className="flex-1" />
     </View>
   );
 };
